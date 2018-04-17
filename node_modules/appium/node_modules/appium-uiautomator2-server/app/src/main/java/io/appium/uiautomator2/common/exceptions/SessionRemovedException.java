bf482ca61a767330232f782b208a7e3c41afc4a6
thyxkris <krisphone@gmail.com>
2018-04-17 11:34:46 +1000

    remove node_modules

--- node_modules/appium/node_modules/appium-uiautomator2-server/app/src/main/java/io/appium/uiautomator2/common/exceptions/SessionRemovedException.java
+++
@@ -1,7 +0,0 @@
-package io.appium.uiautomator2.common.exceptions;
-
-public class SessionRemovedException extends UiAutomator2Exception{
-    public SessionRemovedException(String message) {
-        super(message);
-    }
-}
--- node_modules/appium/node_modules/appium-uiautomator2-server/app/src/main/java/io/appium/uiautomator2/http/IHttpServlet.java
+++
@@ -1,5 +0,0 @@
-package io.appium.uiautomator2.http;
-
-public interface IHttpServlet {
-    public void handleHttpRequest(io.appium.uiautomator2.http.IHttpRequest IHttpRequest, io.appium.uiautomator2.http.IHttpResponse httpResponse) throws Exception;
-}
